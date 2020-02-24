47e687c653bf2b86c489eb40e4077a5c2343c7e8
vibhor-rastogi247 <vibhor.rastogi247@gmail.com>
2020-02-24 20:27:13 +0530

    na

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeadderComponent } from './Components/headder/headder.component';
+import { BasicComponent } from './Components/basic/basic.component';
+import { FormComponent } from './Components/form/form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'home', 
+    component:  BasicComponent
+  },
+  { path: 'form', 
+  component:  FormComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
