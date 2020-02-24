9bebdd08b9b38fc41949c8f7d95b535ed49bf6fd
vibhor-rastogi247 <vibhor.rastogi247@gmail.com>
2020-02-24 16:38:24 +0530

    seconf

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BasicComponent } from './Component/basic/basic.component';
+import { FormComponent } from './Component/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
